32c2fb9b7efd3580d9362df7db66200b173e5074
sofia123-diop <sofiadiop01@gmail.com>
2026-05-08 12:00:25 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+st.title("Safietou Abdou Aziz Diop")
+
+st.header("Technicienne supérieure en Sciences et Techniques Agricoles, Alimentaires et Nutritionnelles")
+st.write("___________________________________________________________________________________________________")
+st.markdown("Intéressée par l’agriculture et son rôle dans l’amélioration de la nutrition, je suis organisée,motivée et j’aime travailler à la fois de manière autonome et en équipe. Mon objectif est de devenir ingénieure agronome et de contribuer à l’amélioration de l’agriculture et de la sécurité alimentaire dans mon pays.")
+
+st.sidebar.markdown("## 💁🏾‍♀️Informations personnelles")
+st.sidebar.image("WhatsApp Image 2026-04-30 at 21.46.18.jpeg", width=190)
+st.sidebar.markdown("📧 Email: sofiadiop01@gmail.com")
+st.sidebar.markdown("📍Adresse: Dakar, Sénégal")
+
+st.sidebar.markdown("## 🌍 Langues")
+st.sidebar.markdown("🇫🇷 Français (C1)")
+st.sidebar.markdown("🇬🇧 Anglais (B1)")
+st.sidebar.markdown("🇸🇳 Wolof (Native)")
+
+st. subheader ("FORMATIONS")
+st.write ("Licence en Sciences et Techniques Agricoles, Alimentaire et Nutritionnelles")
+st.write("En cours")
+st.write ("BACCALAUREAT SCIENTIFIQUE")
+st.write("2023-2024")
+st. subheader ("EXPERIENCES PROFESSIONNELLES")
+with st.expander("Stage au Centre de Formation Professionnelle Horticole de Dakar (CFPH) Durée : 1er Novembre 2025 - 30 Novembre 2025  "):
+     st.markdown("* Participation aux activités pratiques de formation agricole")
+     st.markdown(" * Apprentissage des techniques de base de production végétale et d’entretien des plantes")
+     st.markdown("* Assistance aux travaux de terrain et à l’entretien de l’exploitation")
+     st.markdown("* Observation et application de pratiques agricoles durables")
+     st.markdown("* Développement de l’esprit d’équipe et des compétences pratiques en agriculture")
+st. subheader ("COMPETENCES ET LOGICIELS")
+st.subheader("Competences")
+st.markdown(" * Production végétale (cultures maraîchères)")
+st.markdown("* Techniques d’entretien et de soin des plantes")
+st.markdown("* Notions en agriculture durable")
+st.markdown(" * Travaux de terrain (semis, entretien, suivi des cultures)")
+st.markdown("* Bases en gestion des cultures")
+st.markdown("* Connaissances en agronomie générale")
+with st.expander("Logiciels"):
+     st.markdown("* Suites bureautiques")
+    
